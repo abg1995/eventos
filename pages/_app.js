9c1bd828f80b12e5c37c19c91ecf09a6e9fb2b85
alejandro <aleblancogovantes@hotmail.com>
2023-08-30 12:54:13 +0300

    changed home to Home

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import MainLayout from '@/src/components/layout/main-layout'
+import MainLayout from '../src/components/layout/main-layout'
 import '../styles/globals.css'
-import '../styles/home.module.css'
+import '../styles/Home.module.css'
 
 export default function App({ Component, pageProps }) {
   return (
