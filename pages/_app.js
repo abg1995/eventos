8b9b6a611df0767fa68718539f7c1f5304cfc0d4
alejandro <aleblancogovantes@hotmail.com>
2023-08-30 12:47:35 +0300

    modified styles route for deployment test, changed @ for ../

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import MainLayout from '@/src/components/layout/main-layout'
-import '@/styles/globals.css'
-import '@/styles/home.module.css'
+import '../styles/globals.css'
+import '../styles/home.module.css'
 
 export default function App({ Component, pageProps }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-import {HomePage} from "@/src/components/home/home-page";;
+import styles from "../styles/Home.module.css";
+import {HomePage} from "../src/components/home/home-page";;
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home({ data }) {
