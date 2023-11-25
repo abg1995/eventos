2994485780511fe4e998108f6dfa863c89d45212
alejandro <aleblancogovantes@hotmail.com>
2023-11-25 19:06:12 -0400

    undid changes

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,14 +1,14 @@
 import Link from "next/link";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-//const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 import Image from "next/image";
 import '/public/logog.webp'
 
 
 export default function Navbar() {
   return (
-    <div className={`${styles.main} `}>
+    <div className={`${styles.main} ${inter.className}`}>
      <Link href="/"><Image src={'/logog.webp'} height={50} width={50}/> </Link> 
       <Link href="/"> Home</Link>
       <Link href="/events"> Events</Link>
